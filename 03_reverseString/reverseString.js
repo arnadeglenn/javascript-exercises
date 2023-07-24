c40aecff9aec278ebff7070749ee90521f432bbf
Glenn Arnade <125632656+arnadeglenn@users.noreply.github.com>
2023-07-23 22:38:28 -0700

    All JS exercises loop, functions, etc

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    const myArray = phrase.split ('');
+    const arrayReverse = myArray.reverse();
+    let reversePhrase = arrayReverse.join('');
+    return reversePhrase
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (min < 0 || max < 0) return "ERROR";
+    if (Number.isInteger(min) === false || Number.isInteger(max)=== false) return "ERROR";
+    if (max < min) {
+        let temp = min;
+        min = max;
+        max = temp;
+    }
+    let sumInt = 0;
+    for (let i = min; i < max + 1; i++) {
+        sumInt += i;
+    }
+    return sumInt;
 };
 
+
+//
 // Do not edit below this line
 module.exports = sumAll;
