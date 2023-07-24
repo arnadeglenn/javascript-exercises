ff82d19cfac37dd94cfbc7bfc3eb4735b57a0d50
Glenn Arnade <125632656+arnadeglenn@users.noreply.github.com>
2023-07-23 22:04:03 -0700

    JS exercises to practice loops,functions,NPM tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fTemp) {
+  let longCelcius = (fTemp -32) * (5/9);
+  let celcius = Math.round(longCelcius*10) / 10;
+  return celcius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cTemp) {
+  let longFahr= (cTemp * (9/5)) +32;
+  let Fahr = Math.round(longFahr*10) / 10;
+  return Fahr;
 };
 
 // Do not edit below this line
